5ea7c61a2e39cb3d503d4b2d2167bd96bbaae99e
Jonata Rossanttles <105800872+JonataRossanttles@users.noreply.github.com>
2024-07-23 17:16:28 -0300

    Add files via upload

--- src/components/curso.js
+++ src/components/curso.js
@@ -29,10 +29,12 @@ sr.reveal(".container-central-curso",{
     <p className='text-curso' >Aprenda os melhores cortes com as melhores técnicas do mercado. Para você que está 
       iniciando e deseja se tornar um barbeiro de excelência, você encontrou o lugar certo.
     </p>
+    <a href="https://wa.me/message/JI4QXVDFI6B7F1" target="_blank" className="link">
     <div className='button-curso'>
       <img src='/imagens/whatsapp.png' alt='' className='img-whats'></img>
-     <a href="https://wa.me/message/JI4QXVDFI6B7F1" target="_blank" className="link"> <span className='text-whats'>Atendimento via Whats</span> </a> 
+      <span className='text-whats'>Atendimento via Whats</span> 
     </div>
+    </a> 
   </div>
     </div>
 
