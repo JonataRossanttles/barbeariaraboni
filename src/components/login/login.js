bee8f629e031c72d85cdc6cce2b2a8b733e0f9b7
Jonata Rossanttles <105800872+JonataRossanttles@users.noreply.github.com>
2025-05-08 14:43:42 -0300

    Update login.js

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -75,7 +75,7 @@ useEffect(()=>{
 // Verificar se o token está valido. 
 function verificy(){
     const  token = localStorage.getItem('tokenid')
-    fetch('https://backendbarbeariaraboni-1.onrender.com/login/consulta',{method:'POST',
+    fetch('https://backendbarbeariaraboni.onrender.com/login/consulta',{method:'POST',
      headers:{'Content-Type': 'Application/json','authorization':token}}).
     then(response=>{if(!response.ok){
         return response.json().then(erro=> {throw new Error(erro.message)} 
